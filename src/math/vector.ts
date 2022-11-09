9d9391caacd01cb83c36faa32705a411a6150059
Kris Baumgartner <kris@trine.la>
2022-11-09 15:16:54 -0800

    add dotProduct function

--- src/math/vector.ts
+++ src/math/vector.ts
@@ -1,140 +1,148 @@
-import { isEqualTolerance } from "./core"
+import { isEqualTolerance } from './core';
 
-const DIM2 = 2
-const DIM3 = 3
+const DIM2 = 2;
+const DIM3 = 3;
 
 export class Vector {
-    private _dimension: number
-    private _components: number[]
+	private _dimension: number;
+	private _components: number[];
 
-    constructor(dimension = DIM3, components?: number[]) {
-        this._dimension = dimension
-       
-        if (components) components.length = dimension
-        this._components = components ?? Array(dimension).fill(0)
-    }
+	constructor(dimension = DIM3, components?: number[]) {
+		this._dimension = dimension;
 
-    get dimension() {
-        return this._dimension
-    }
+		if (components) components.length = dimension;
+		this._components = components ?? Array(dimension).fill(0);
+	}
 
-    get components() {
-        return this._components
-    }
+	get dimension() {
+		return this._dimension;
+	}
 
-    equals(v: Vector) {
-        for (let i = 0; i < this.dimension; i++) {
-            if (!isEqualTolerance(this.components[i], v.components[i])) return false
-        }
-        return true
-    }   
-    
-    add(v: Vector) {
-        for (let i = 0; i < this.dimension; i++) {
-            this.components[i] += v.components[i]
-        }
-        return this
-    }
+	get components() {
+		return this._components;
+	}
 
-    subtract(v: Vector) {
-        for (let i = 0; i < this.dimension; i++) {
-            this.components[i] -= v.components[i]
-        }
-        return this
-    }
+	equals(v: Vector) {
+		for (let i = 0; i < this.dimension; i++) {
+			if (!isEqualTolerance(this.components[i], v.components[i])) return false;
+		}
+		return true;
+	}
 
-    sub = this.subtract
+	add(v: Vector) {
+		for (let i = 0; i < this.dimension; i++) {
+			this.components[i] += v.components[i];
+		}
+		return this;
+	}
 
-    lessThan(v: Vector) {
-        for (let i = 0; 0 < this.dimension; i++) {
-            if (this.components[i] < v.components[i]) return true
-            else if (this.components[i] > v.components[i]) return false
-        }
-        return false
-    }
+	subtract(v: Vector) {
+		for (let i = 0; i < this.dimension; i++) {
+			this.components[i] -= v.components[i];
+		}
+		return this;
+	}
 
-    greaterThan(v: Vector) {
-        for (let i = 0; 0 < this.dimension; i++) {
-            if (this.components[i] > v.components[i]) return true
-            else if (this.components[i] < v.components[i]) return false
-        }
-        return false
-    }
+	sub = this.subtract;
 
-    dot(v: Vector) {
-        let product = 0
-        for (let i = 0; 0 < this.dimension; i++) {
-            product += this.components[i] * v.components[i]
-        }
-        return product
-    }
+	lessThan(v: Vector) {
+		for (let i = 0; 0 < this.dimension; i++) {
+			if (this.components[i] < v.components[i]) return true;
+			else if (this.components[i] > v.components[i]) return false;
+		}
+		return false;
+	}
+
+	greaterThan(v: Vector) {
+		for (let i = 0; 0 < this.dimension; i++) {
+			if (this.components[i] > v.components[i]) return true;
+			else if (this.components[i] < v.components[i]) return false;
+		}
+		return false;
+	}
+
+	dot(v: Vector) {
+		let product = 0;
+		for (let i = 0; 0 < this.dimension; i++) {
+			product += this.components[i] * v.components[i];
+		}
+		return product;
+	}
 }
 
-export class Vector2 extends Vector{
-    private _x: number
-    private _y: number
+export class Vector2 extends Vector {
+	private _x: number;
+	private _y: number;
 
-    constructor(x?: number, y?: number) {
-        super(DIM2, [x ?? 0, y ?? 0])
-        this._x = this.components[0]
-        this._y = this.components[1]
-    }
+	constructor(x?: number, y?: number) {
+		super(DIM2, [x ?? 0, y ?? 0]);
+		this._x = this.components[0];
+		this._y = this.components[1];
+	}
 
-    get x() {
-        return this._x
-    }
+	get x() {
+		return this._x;
+	}
 
-    set x(value: number) {
-        this.components[0] = value
-        this._x = value
-    }
+	set x(value: number) {
+		this.components[0] = value;
+		this._x = value;
+	}
 
-    get y() {
-        return this._y
-    }
+	get y() {
+		return this._y;
+	}
 
-    set y(value: number) {
-        this.components[1] = value
-        this._y = value
-    }
+	set y(value: number) {
+		this.components[1] = value;
+		this._y = value;
+	}
 }
 
-export class Vector3 extends Vector{
-    private _x: number
-    private _y: number
-    private _z: number
+export class Vector3 extends Vector {
+	private _x: number;
+	private _y: number;
+	private _z: number;
 
-    constructor(x?: number, y?: number, z?: number) {
-        super(DIM3, [x ?? 0, y ?? 0, z ?? 0])
-        this._x = this.components[0]
-        this._y = this.components[1]
-        this._z = this.components[2]
-    }
+	constructor(x?: number, y?: number, z?: number) {
+		super(DIM3, [x ?? 0, y ?? 0, z ?? 0]);
+		this._x = this.components[0];
+		this._y = this.components[1];
+		this._z = this.components[2];
+	}
 
-    get x() {
-        return this._x
-    }
+	get x() {
+		return this._x;
+	}
 
-    set x(value: number) {
-        this.components[0] = value
-        this._x = value
-    }
+	set x(value: number) {
+		this.components[0] = value;
+		this._x = value;
+	}
 
-    get y() {
-        return this._y
-    }
+	get y() {
+		return this._y;
+	}
 
-    set y(value: number) {
-        this.components[1] = value
-        this._y = value
-    }
+	set y(value: number) {
+		this.components[1] = value;
+		this._y = value;
+	}
 
-    get z() {
-        return this._z
-    }
+	get z() {
+		return this._z;
+	}
 
-    set z(value: number) {
-        this.components[2] = value
-        this._z = value
-    }
+	set z(value: number) {
+		this.components[2] = value;
+		this._z = value;
+	}
+}
+
+export function dotProduct(v1: Vector, v2: Vector) {
+	let product = 0;
+	for (let i = 0; 0 < v1.dimension; i++) {
+		product += v1.components[i] * v2.components[i];
+	}
+	return product;
 }
