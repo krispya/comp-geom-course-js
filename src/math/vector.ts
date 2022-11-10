fe9bd93639dff99bad7f6e05a5ab14677524775d
Kris Baumgartner <kjbaumgartner@gmail.com>
2022-11-10 11:55:50 -0800

    Added cross product. Attempted to make better type experience, but have hit a snag

--- src/math/vector.ts
+++ src/math/vector.ts
@@ -3,15 +3,15 @@ import { isEqualTolerance } from './core';
 const DIM2 = 2;
 const DIM3 = 3;
 
-export class Vector {
+type Tuple<T, N extends number, R extends T[] = []> = R['length'] extends N ? R : Tuple<T, N, [T, ...R]>;
+
+export class Vector<T extends number = number> {
 	private _dimension: number;
-	private _components: number[];
+	private _components: Tuple<number, T>;
 
-	constructor(dimension = DIM3, components?: number[]) {
+	constructor(dimension: T, components?: Tuple<number, T>) {
 		this._dimension = dimension;
-
-		if (components) components.length = dimension;
-		this._components = components ?? Array(dimension).fill(0);
+		this._components = components ?? (Array(dimension).fill(0) as Tuple<number, T>);
 	}
 
 	get dimension() {
@@ -22,21 +22,21 @@ export class Vector {
 		return this._components;
 	}
 
-	equals(v: Vector) {
+	equals(v: Vector<T>) {
 		for (let i = 0; i < this.dimension; i++) {
 			if (!isEqualTolerance(this.components[i], v.components[i])) return false;
 		}
 		return true;
 	}
 
-	add(v: Vector) {
+	add(v: Vector<T>) {
 		for (let i = 0; i < this.dimension; i++) {
 			this.components[i] += v.components[i];
 		}
 		return this;
 	}
 
-	subtract(v: Vector) {
+	subtract(v: Vector<T>) {
 		for (let i = 0; i < this.dimension; i++) {
 			this.components[i] -= v.components[i];
 		}
@@ -45,7 +45,7 @@ export class Vector {
 
 	sub = this.subtract;
 
-	lessThan(v: Vector) {
+	lessThan(v: Vector<T>) {
 		for (let i = 0; 0 < this.dimension; i++) {
 			if (this.components[i] < v.components[i]) return true;
 			else if (this.components[i] > v.components[i]) return false;
@@ -53,7 +53,7 @@ export class Vector {
 		return false;
 	}
 
-	greaterThan(v: Vector) {
+	greaterThan(v: Vector<T>) {
 		for (let i = 0; 0 < this.dimension; i++) {
 			if (this.components[i] > v.components[i]) return true;
 			else if (this.components[i] < v.components[i]) return false;
@@ -61,7 +61,7 @@ export class Vector {
 		return false;
 	}
 
-	dot(v: Vector) {
+	dot(v: Vector<T>) {
 		let product = 0;
 		for (let i = 0; 0 < this.dimension; i++) {
 			product += this.components[i] * v.components[i];
@@ -70,72 +70,61 @@ export class Vector {
 	}
 }
 
-export class Vector2 extends Vector {
-	private _x: number;
-	private _y: number;
-
+export class Vector2 extends Vector<2> {
 	constructor(x?: number, y?: number) {
 		super(DIM2, [x ?? 0, y ?? 0]);
-		this._x = this.components[0];
-		this._y = this.components[1];
 	}
 
 	get x() {
-		return this._x;
+		return this.components[0];
 	}
 
 	set x(value: number) {
 		this.components[0] = value;
-		this._x = value;
 	}
 
 	get y() {
-		return this._y;
+		return this.components[1];
 	}
 
 	set y(value: number) {
 		this.components[1] = value;
-		this._y = value;
 	}
 }
 
-export class Vector3 extends Vector {
-	private _x: number;
-	private _y: number;
-	private _z: number;
-
+export class Vector3 extends Vector<3> {
 	constructor(x?: number, y?: number, z?: number) {
 		super(DIM3, [x ?? 0, y ?? 0, z ?? 0]);
-		this._x = this.components[0];
-		this._y = this.components[1];
-		this._z = this.components[2];
 	}
 
 	get x() {
-		return this._x;
+		return this.components[0];
 	}
 
 	set x(value: number) {
 		this.components[0] = value;
-		this._x = value;
 	}
 
 	get y() {
-		return this._y;
+		return this.components[1];
 	}
 
 	set y(value: number) {
 		this.components[1] = value;
-		this._y = value;
 	}
 
 	get z() {
-		return this._z;
+		return this.components[2];
 	}
 
 	set z(value: number) {
 		this.components[2] = value;
-		this._z = value;
+	}
+
+	set(x: number, y: number, z: number) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
 	}
 }
 
@@ -145,4 +134,16 @@ export function dotProduct(v1: Vector, v2: Vector) {
 		product += v1.components[i] * v2.components[i];
 	}
 	return product;
+}
+
+export function crossProduct2D(v1: Vector2, v2: Vector2) {
+	return v1.x * v2.y - v1.y * v2.x;
+}
+
+export function crossProduct3D(v1: Vector3, v2: Vector3, target: Vector3) {
+	const x = v1.y * v2.z - v1.z * v2.y;
+	const y = v1.z * v2.x - v1.x * v2.z;
+	const z = v1.x * v2.y - v1.y * v2.x;
+
+	return target ? target.set(x, y, z) : new Vector3(x, y, z);
 }
